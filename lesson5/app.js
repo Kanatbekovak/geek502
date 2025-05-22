a950279de4aca4283607c2d9c12344e5a678734a
Kanatbekova Kaniet <kanatbekovakaniet@gmail.com>
2025-05-22 20:46:34 +0600

    lesson5

---
+++ lesson5/app.js
@@ -0,0 +1,110 @@
+console.log('Hello')
+alert('hello world')
+
+let customer:string = 'John';
+let customerName:string = 'Bakit';
+let customer_name:string = 'Kuban';
+let customerSurname:string = 'Sariev'
+let $:string = '12';
+let $num:string = '12';
+let _num:number = '12';
+let num123:number = '123'
+// let 45 = sdfg
+// let 234wdfwe = wdw
+// let var = 123
+let age:number = 18
+console.log('Имя=',customer,'','Фамилия=',customerName, 'Возраст=',age);
+console.log(`Имя=${customer} Фамилия=${customerSurname} Возраст=${age}`);
+console.log("OAO \"еверэлектро\"");
+let file:string = 'd:\\data\\profile.txt'
+console.log(file);
+let number:number = 123
+let number1:number = 3.14
+console.log(number);
+number = 500
+console.log(number);
+let a = 10
+let b = 4
+console.log(a + b,'+');
+console.log(a - b,'-');
+console.log(a * b,'*');
+console.log(a % b,'%');
+console.log(5 % 2,'5%');
+console.log(4 % 2,'4%');
+console.log(a / b,'/');
+console.log(a ** b,'**');
+console.log(a/ 0);
+console.log(0/ a);
+console.log(435544 + 'abfs%');
+console.log("435544" + 456);
+console.log("435544" * 456);
+a = 10
+b = '10'
+console.log(a>b,'a>b');
+console.log(a<b,'a<b');
+console.log(a>=b,'a>=b');
+console.log(a<=b,'a<b');
+console.log(a==b,'a==b');
+console.log(a===b,'a===b');
+console.log(a!==b,'a!==b');
+console.log(a!=b,'a!=b');
+console.log(true && false,'&& и');
+console.log(true || false,'|| или');
+console.log(!true,'! не');
+console.log((true && false) || true);
+console.log(!(true && false) && true);
+console.log('' || 'awda');
+console.log(0 || 'awda');
+let x:number = 5
+x=x+3 // x +=3
+console.log(x,'8');
+x -= 2
+let y:number = 1
+console.log(y++);
+console.log(y--)
+console.log(y)
+let by:number = 1
+console.log(++by);
+console.log(--by);
+
+
+console.log(typeof y)
+console.log(typeof 'y')
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+let df
+console.log(df)
+let dfd:number = 34
+console.log(dfd)
+let empty:null = null
+console.log( typeof empty)
+console.log( typeof {empty})
+
+let color = prompt('сколько вам лет')
+console.log(color,'age1')
+console.log( typeof color,'typeof age1')
+
+if (color==='red') {
+    console.log('red')
+} else {
+    console.log('false')
+}
+
+// let age1 = prompt('сколько вам лет')
+// console.log(age1,'age1')
+// console.log( typeof age1,'typeof age1')
+
+// if (age1>1000) {
+//     console.log(' ВЫ бессмертны')
+// } elseif (age1>18) {
+//     console.log('Вы совершеннолетний')
+// } elseif (age1>0) {
+//     console.log('Вы нге совершеннолетний')
+// } else {
+//     console.log('ВЫ не правильно указали возраст')
+// }
+
+
+
+
